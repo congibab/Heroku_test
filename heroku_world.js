e0da4ed7e7b45e3ec560bb69a4ef2425b2f1546f
congibab <engljapen2@gmail.com>
2021-01-02 13:10:41 +0900

    second commit

--- heroku_world.js
+++ heroku_world.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
- 
+const PORT = process.env.PORT
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-app.listen(3000)
+app.listen(PORT)
