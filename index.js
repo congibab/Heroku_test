a3077b2ede57ef0527f14bff0d9793c1ad3ba957
congibab <engljapen2@gmail.com>
2021-01-11 22:15:50 +0900

    server heroku upload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+
 //var consola = require('consola');
 //=======================================a
 //=======================================
@@ -14,7 +15,7 @@ var Clients = [];
 var Rooms = [];
 var Massages = [];
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 5000, () => {
 	console.log('URL = ws://localhost:3000/socket.io/?EIO=4&transport=websocket');
 	//consola.success('URL = ws://localhost:3000/socket.io/?EIO=4&transport=websocket');
 });
